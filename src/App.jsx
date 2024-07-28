c2d279a403d6040e28181c60f6f3b6e14fc5b90f
Mohammad <mohammad.ghorbani@hotmail.com>
2024-07-28 13:23:06 +0330

    feat: adds footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './Header'
+import Footer from './Footer'
 import './App.css'
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
   )
 }
